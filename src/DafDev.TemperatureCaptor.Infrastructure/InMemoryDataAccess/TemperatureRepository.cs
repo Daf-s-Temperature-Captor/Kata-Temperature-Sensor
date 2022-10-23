d2ec11dad568c292cd735eda1a638e02ed951cb7
Daf <afahd.ali@gmail.com>
2022-10-23 16:58:51 +0200

    implemented infrastructure and added in memory data

---
+++ src/DafDev.TemperatureCaptor.Infrastructure/InMemoryDataAccess/TemperatureRepository.cs
@@ -0,0 +1,35 @@
+using DafDev.TemperatureCaptor.Domain.Sensor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DafDev.TemperatureCaptor.Infrastructure.InMemoryDataAccess;
+public class TemperatureRepository : ICaptureTemperatures
+{
+    private readonly Random _random;
+    private readonly Context _context;
+
+    public TemperatureRepository(Context context)
+    {
+        _context = context;
+        _random = new Random();
+    }
+
+    public async Task<double> GetTemperature()
+    {
+        //We generate a random temperature between -40 and 60 °C
+        var temperature = _random.NextDouble()*100 - 40;
+        _context.Temperatures.Add(temperature);
+        return await Task.FromResult(temperature);
+    }
+
+    public async Task<IEnumerable<double>> GetTemperatures(int numberOfMeasures = 15)
+    {
+        if(numberOfMeasures > _context.Temperatures.Count)
+            numberOfMeasures = _context.Temperatures.Count;
+
+        return await Task.FromResult(_context.Temperatures.TakeLast(numberOfMeasures));
+    }
+}
